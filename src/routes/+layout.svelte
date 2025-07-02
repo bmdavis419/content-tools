5a80e21fc9db786ca7a9c49608fa325472daddfc
Benjamin Davis <davis.benjamin41902@gmail.com>
2025-07-01 18:34:30 -0700

    tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import "../app.css";
+
+  const { children } = $props();
+</script>
+
+<main class="w-full h-screen bg-neutral-900 text-white">
+  <div class="flex grow overflow-scroll">
+    {@render children()}
+  </div>
+</main>
