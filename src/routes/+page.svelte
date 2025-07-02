31dcefe4e8923d8b6f1e3f1aa241aeb75ffd12e1
Benjamin Davis <davis.benjamin41902@gmail.com>
2025-07-01 18:39:41 -0700

    added shadcn svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+  import Button from "../lib/components/ui/button/button.svelte";
+</script>
+
+<Button onclick={() => console.log("fuck")}>fuck</Button>
+
 <div>this is main page :0</div>
 
 <h2>fuck</h2>
